562b07d8b84cea5ff83b4da210eb7f1e6a7e70fb
Shin Maung Maung <ethereal97@gmail.com>
2024-10-02 00:17:45 +0700

    add more information in registrants

--- admin/registrants.js
+++ admin/registrants.js
@@ -16,4 +16,20 @@ requireAuth().then(() => {
       });
     },
   });
+
+  function generate(user_input) {
+    let qr_code_element = document.querySelector(".qr-code");
+
+    qr_code_element.style = "";
+
+    new QRCode(qr_code_element, {
+      logo: "./assets/logo.png",
+      text: user_input,
+      width: 350,
+      height: 350,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.L,
+    });
+  }
 });
